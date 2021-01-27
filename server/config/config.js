212b2dd06c9df9167f266be63e672f6f536dd206
Fernando <fernandosierra.st@gmail.com>
2021-01-27 20:50:47 -0300

    login normal

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,3 @@
-
 //=================
 //=====PUERTO======
 //=================
@@ -7,16 +6,30 @@ process.env.PORT = process.env.PORT || 3000;
 //=================
 // ====ENTORNO====
 //=================
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//=================
+//VENCIMIENTO TOKEN
+//=================
+// 60 SEGUNDOS
+// 60 MINUTOS
+// 24 HORAS
+// 30 DIAS
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30
+
+
+//=====================
+//SEED de autenticacion
+//=====================
+process.env.SEED = process.env.SEED || 'secret-desarrollo'
+
 
 
 //=================
 //==BASE DE DATOS==
 //=================
 const urlDB = (process.env.NODE_ENV === 'dev') ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URI
-  
-
 process.env.URLDB = urlDB;
 
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdminRole}= require('../middlewares/autenticacion')
 const app = express();
 
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -32,7 +33,7 @@ app.get('/usuario', function (req, res) {
         })
 
 })
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificaToken,verificaAdminRole], (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
@@ -54,7 +55,7 @@ app.post('/usuario', function (req, res) {
         })
     });
 });
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id', [verificaToken,verificaAdminRole], (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
@@ -71,7 +72,7 @@ app.put('/usuario/:id', function (req, res) {
         });
     })
 });
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id', [verificaToken,verificaAdminRole], (req, res) => {
     let id = req.params.id;
 
     //  Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
