0ce105c5f82bcf8d0d286a07df774433904dc8e1
Fernando <fernandosierra.st@gmail.com>
2021-01-30 02:59:51 -0300

    subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -40,10 +40,28 @@ let verificaAdminRole = (req, res, next) => {
     }
 
 }
+//=====================
+//Verifica token en imagen
+//=====================
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if(err){
+            return res.status(401).json({
+                ok : false,
+                err: {
+                    message: 'token no valido'
+                }
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
 
-
+    });
+}
 
 module.exports = {
     verificaToken,
-    verificaAdminRole
+    verificaAdminRole,
+    verificaTokenImg
 };
